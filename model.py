6aa166d81008e1ede15c25eba86d4a2094a1eac6
orbb4 <114522263+orbb4@users.noreply.github.com>
2024-07-14 11:23:26 -0400

    modelo con dropout ajustado

--- model.py
+++ model.py
@@ -1,36 +1,31 @@
 import numpy as np
 from keras import layers, models
-
-
-def kociemba_to_onehot(kociemba_str: str) -> np.ndarray:
-    """
-    Traduce una string kociemba a la secuencia correspondiente
-    de acciones.
-    """
+def kociemba_to_onehot(kociemba_str):
     color_mapping = {
-        "B": [1, 0, 0, 0, 0, 0],
-        "L": [0, 1, 0, 0, 0, 0],
-        "F": [0, 0, 1, 0, 0, 0],
-        "U": [0, 0, 0, 1, 0, 0],
-        "R": [0, 0, 0, 0, 1, 0],
-        "D": [0, 0, 0, 0, 0, 1],
+        'B': [1, 0, 0, 0, 0, 0],
+        'L': [0, 1, 0, 0, 0, 0],
+        'F': [0, 0, 1, 0, 0, 0],
+        'U': [0, 0, 0, 1, 0, 0],
+        'R': [0, 0, 0, 0, 1, 0],
+        'D': [0, 0, 0, 0, 0, 1]
     }
 
-    onehot_vec = np.concatenate([color_mapping[char] for char in kociemba_str])
+    onehot_vec = np.concatenate([color_mapping[char] for char in kociemba_str], dtype=np.int8)
 
     return onehot_vec
-
 
 patterns = []
 steps = []
 
 
-with open("./data/cube2M.txt", "r") as file:
+with open("./data/combined_data.txt", 'r') as file:
     for line in file:
         line = line.strip()
         if line:
-            pattern, step_count = line.split("-")
-            step_count = min(int(step_count) + 1, 20)
+            pattern, step_count = line.split('-')
+            step_count = str(int(step_count))
+            if int(step_count) > 20:
+                step_count = "20"
 
             patterns.append(kociemba_to_onehot(pattern))
             steps.append(int(step_count))
@@ -40,15 +35,19 @@ y = np.array(steps)
 
 model = models.Sequential()
 model.add(layers.Flatten(input_shape=(len(X[0]),)))  # Aplanar la entrada
-model.add(layers.Dense(1024, activation="relu"))
-model.add(layers.Dropout(0.5))
-model.add(layers.Dense(512, activation="relu"))
-model.add(layers.Dropout(0.5))
+model.add(layers.Dense(1024, activation='relu'))
+model.add(layers.Dropout(0.3))
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dropout(0.3))
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dropout(0.3))
 model.add(layers.Dense(1, activation="relu"))
 
 
-model.compile(optimizer="adam", loss="mean_squared_error", metrics=["mae"])
+model.compile(optimizer='adam',
+              loss='mean_squared_error',
+              metrics=['mae'])
 
 
 model.fit(X, y, epochs=50, batch_size=128, validation_split=0.2)
-model.save("./rubik_solver_model2Mrelu.h5")
+model.save('./solverB.h5')
